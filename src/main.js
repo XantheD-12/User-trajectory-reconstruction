12b133edf28ee614b281919009665d5944d554b1
XantheD-12 <1043317674@qq.com>
2022-05-01 21:26:25 +0800

    update config
    更新了一些配置和依赖文件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as echarts from 'echarts'
+
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
+
+import AmapVue from '@amap/amap-vue';
+AmapVue.config.version = '2.0'; // 默认2.0，这里可以不修改
+AmapVue.config.key = 'f93bc0130ba2e65581dd067f234e8adc';
+AmapVue.config.plugins = [
+  'AMap.ToolBar',
+  'AMap.MoveAnimation',
+  // 在此配置你需要预加载的插件，如果不配置，在使用到的时候会自动异步加载
+];
+Vue.use(AmapVue);
+
+import VueAMap from 'vue-amap'
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+  key: 'your amap key',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 默认高德 sdk 版本为 1.4.4
+  v: '1.4.4'
+});
+
 
 new Vue({
   router,
