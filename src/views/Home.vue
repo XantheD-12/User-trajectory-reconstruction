0393ffda9f1e2ad059e7ae004448cf85c97e0de2
XantheD-12 <1043317674@qq.com>
2022-05-02 15:23:22 +0800

    update ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div style="width: 100%; height: 400px">
+      <amap :zoom="10" :center="center">
+        <amap-marker
+          v-for="(pos, i) in res"
+          :key="i"
+          :position="pos"
+          :label="{
+            content: i + 'pos',
+            direction: 'bottom',
+          }"
+        />
+      </amap>
+    </div>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      msg: "Hello, AmapVue!",
+      center: [116.407387, 39.904179],
+      results: [
+        [116.473179, 39.993169],
+        [116.234246, 40.044793],
+      ],
+      res: [],
+    };
+  },
+  methods: {
+    getpoints() {},
+    gethello() {
+      // 测试后端接口
+      // this.$axios({
+      //   method: "get",
+      //   url: "origin",
+      // })
+      //   .then((res) => {
+      //     console.log(res);
+      //     console.log(res.data);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+      this.$axios({
+        method: "get",
+        url: "result",
+      })
+        .then((res) => {
+          // console.log(res.data);
+          this.res = res.data[0];
+          // 过滤非空的
+          this.res = this.res.filter((data) => {
+            return data != "";
+          });
+          console.log(this.res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  created() {
+    this.gethello();
+  },
+};
 </script>
+
+<style lang="sass" scoped>
+</style>
