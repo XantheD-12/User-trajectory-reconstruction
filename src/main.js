12b57a66c074b22b728ec90fe6cfd941996647d6
XantheD-12 <1043317674@qq.com>
2022-05-03 23:57:56 +0800

    update config
    增加、删除了一些依赖

--- src/main.js
+++ src/main.js
@@ -2,24 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import * as echarts from 'echarts'
 import axios from 'axios'//引入axios
-import qs from 'qs'
-import vuetify from './plugins/vuetify'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
 
-Vue.prototype.$echarts = echarts
+Vue.prototype.$axios = axios
 
-// 解决跨越问题
-// 配置代理 config 下的index.js 文件
-// 可以参考：https://juejin.cn/post/6844903718085722119
-Vue.prototype.$axios = axios.create({
-  // 本地
-  // baseURL:'http://localhost:8003/',
-  // 服务器
-  baseURL:'http://127.0.0.1:5000/',
-});//把axios挂载到vue上,使用方法为:this.$axios
-Vue.prototype.$qs = qs //解决axios的post问题
-
+window._AMapSecurityConfig = {
+  securityJsCode:"c36025964916f58d0c51d412f23fd016" // 您申请的安全密钥
+};
 import AmapVue from '@amap/amap-vue';
 AmapVue.config.version = '2.0'; // 默认2.0，这里可以不修改
 AmapVue.config.key = 'f93bc0130ba2e65581dd067f234e8adc';
@@ -28,6 +18,7 @@ AmapVue.config.plugins = [
   'AMap.MoveAnimation',
   // 在此配置你需要预加载的插件，如果不配置，在使用到的时候会自动异步加载
 ];
+
 Vue.use(AmapVue);
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,30 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 // 参考：https://cli.vuejs.org/zh/config/#vue-config-js
- module.exports = {
+module.exports = {
   // 选项...
+  // proxy: {
+  //   '/api': {
+  //     target: 'http://127.0.0.1:5000',  // 后台接口域名
+  //     ws: true,        //如果要代理 websockets，配置这个参数
+  //     secure: false,  // 如果是https接口，需要配置这个参数
+  //     changeOrigin: true,  //是否跨域
+  //   }
+  // }
+  //assetsDir: 'public', //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  //publicPath: '/', //部署应用包时的基本 URL。  
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true, //是否跨域
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    host: 'localhost', //
+    port: 8080, //
+  },
+
 }
